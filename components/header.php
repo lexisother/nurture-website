bbc57119b814546a00a0a1efe6562fca213279c0
Alyxia Sother <alyxia@riseup.net>
2022-03-10 20:53:15 +0000

    [components/header] Add basic navbar

---
+++ components/header.php
@@ -0,0 +1,9 @@
+<nav>
+  <div class="title">
+    Title
+  </div>
+  <div class="links">
+    <a>test 1</a>
+    <a>test 2</a>
+  </div>
+</nav>
--- home.php
+++ home.php
@@ -1,2 +1,5 @@
 <?php
+// TODO: Move this to a `base` template or something. Nobody likes `include`ing base
+// files every time you make a page.
 include __DIR__ . '/components/head.php';
+include __DIR__ . '/components/header.php';
