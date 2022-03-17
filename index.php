134d9304b747389f89a3abad0b8b6b1b6e4d9c1c
Alyxia Sother <alyxia@riseup.net>
2022-03-17 20:58:17 +0000

    [index] Add the Whoops error handler

--- index.php
+++ index.php
@@ -3,12 +3,20 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Bramus\Router\Router;
+use Whoops\Run;
+use Whoops\Handler\PrettyPageHandler;
 # https://packagist.org/packages/knplabs/github-api
 # https://packagist.org/packages/league/commonmark
 
 // You know, I was thinking of going completely overboard and writing my own
 // controller-structure using classes.
 // Maybe I should. I probably shouldn't. But I could.
+
+// TODO" Maybe write my own. Although, this looks a lot like old Ignition and
+// that makes me happy.
+$whoops = new Run;
+$whoops->pushHandler(new PrettyPageHandler);
+$whoops->register();
 
 $router = new Router();
 $posts = include 'markdown.php';
