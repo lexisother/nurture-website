301bfea90af08b2ed304483dd1b96ab55fb8f007
Alyxia Sother <alyxia@riseup.net>
2022-03-25 09:55:39 +0100

    [views/songs] Add grid of songs

---
+++ views/songs.php
@@ -0,0 +1,33 @@
+<?php
+includeWithVariables(__DIR__ . '/../templates/base.php', ['pageTitle' => 'Songs']);
+$title = "Songs";
+$description = "A list of Nurture's songs, fetched from the Spotify API.";
+
+$file = file_get_contents(__DIR__ . '/../assets/songs.json');
+$json = json_decode($file);
+
+?>
+
+<div class="tour-page">
+  <div class="header">
+    <div class="title"><?= $title ?></div>
+    <div class="desc"><?= $description ?></div>
+  </div>
+  <div class="grid">
+    <?php
+    foreach ($json->items as $track) {
+      $meta = array_column($json->items, null, 'name')[$track->name];
+    ?>
+      <div class="gridItem">
+        <div class="gridItemHeader">
+          <iframe src="https://open.spotify.com/embed/track/<?= $track->id ?>" width="457" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        </div>
+        <div class="gridItemText">
+          <p><a href="/songs/<?= str_replace(" ", "-", strtolower($track->name)) ?>"><?= $track->name ?></a></p>
+        </div>
+      </div>
+    <?php
+    }
+    ?>
+  </div>
+</div>
