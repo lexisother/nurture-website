ce24acdc50b375f2a2ee9a65357a76adbc04a422
Alyxia Sother <alyxia@riseup.net>
2022-03-25 11:46:49 +0100

    [views/home] Add some homepage content

--- views/home.php
+++ views/home.php
@@ -9,4 +9,14 @@ $description = "";
     <div class="title"><?= $title ?></div>
   </div>
 
+  <p>Welcome to my informational website about my favourite album ever published, Porter Robinson's "Nurture."</p>
+  <p>I first encountered the very first song "Get Your Wish", in the 2021 stay-at-home online edition of Porter's yearly festival, "Second Sky" (titled "Secret Sky" during COVID.)</p>
+  <p>I instantly fell in love. The melodies, the mastering, the vocals, everything about it just felt so... comforting, somehow.</p>
+  <p>As more of the tracks released, such as "Look at the Sky" at the end of the live set, the album quickly made its way into my heart.</p>
+  <p>Ever since, I have not stopped listening to the album, remixes of it, or his live sets.</p>
+  <p>I always had a liking for Porter's music, such as his debut album "Worlds", where tracks like "Flicker" or "Hear the Bells" jumped out to me.</p>
+  <p>But Nurture? It's something special.</p>
+  <br />
+  <p><i>"Everything we need is already here."</i></p>
+
 </div>
